6316b1f9d5872f6868b617eb7267fc109c8eb713
Logan Sutton <logan.w.sutton@wsu.edu>
2025-08-04 18:59:16 -0700

    Enhance World class to include linear noise map and min/max value calculations

--- World.py
+++ World.py
@@ -4,6 +4,13 @@ class World:
     def __init__(self, size_x, size_y, seed):
         self.generate_noisemap(size_x, size_y, seed)
 
+        # Flatten the noise map
+        self.linear_map = [item for sublist in self.noise_map for item in sublist ]
+
+        # Get min and max values
+        self.min_value = min(self.linear_map)
+        self.max_value = max(self.linear_map)
+
     def generate_noisemap(self, size_x, size_y, seed): 
         self.noise_map = []
 
@@ -26,3 +33,6 @@ class World:
                 noise_value += noise4([x/xpix, y/ypix]) * 0.125
                 row.append(noise_value)
             self.noise_map.append(row)
+
+    def get_noise_map(self):
+        return self.noise_map
